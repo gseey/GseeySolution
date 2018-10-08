e879972ee36154999c52341146a606d6387e3186
Gaos <gaos@live.cn>
2018-10-08 21:14:56 +0800

    添加配置文件帮助类 添加控制台测试项目

---
+++ Gseey.ConsoleTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using Gseey.Framework.Common.Helpers;
+using System;
+using System.Threading;
+
+namespace Gseey.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+            while (count<1000)
+            {
+                Thread.Sleep(1000);
+                var confingValue = ConfigHelper.Get("test:t1:t2", "fsfsdfsfsfsdf");
+
+                Console.WriteLine(confingValue);
+            }
+
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Gseey.Framework.Common/Helpers/ConfigHelper.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Gseey.Framework.Common.Helpers
+{
+    public sealed class ConfigHelper
+    {
+        private readonly static IConfiguration config = null;
+
+        #region 静态类,配置对应文件
+
+        static ConfigHelper()
+        {
+            var binder = new ConfigurationBuilder();
+            binder.SetBasePath(Directory.GetCurrentDirectory());
+            binder.AddJsonFile("appsetting.json", optional: true, reloadOnChange: true);
+            config = binder.Build();
+        }
+
+        #endregion
+
+        #region 获取配置文件值
+
+        /// <summary>
+        /// 获取配置文件值
+        /// </summary>
+        /// <param name="key">配置键</param>
+        /// <param name="defaultValue">默认值</param>
+        /// <returns></returns>
+        public static string Get(string key, string defaultValue = "")
+        {
+            return config.GetValue<string>(key, defaultValue);
+        }
+
+
+        /// <summary>
+        /// 获取配置文件值
+        /// </summary>
+        /// <param name="key">配置键</param>
+        /// <param name="defaultValue">默认值</param>
+        /// <returns></returns>
+        public static T Get<T>(string key, T defaultValue = default(T))
+        {
+            return config.GetValue<T>(key, defaultValue);
+        }
+
+        #endregion
+
+        #region 获取连接字符串
+
+        /// <summary>
+        /// 获取连接字符串
+        /// </summary>
+        /// <param name="key">配置键</param>
+        /// <returns></returns>
+        public static string GetConnectionString(string key)
+        {
+            return config.GetConnectionString(key);
+        }
+
+        #endregion
+    }
+}
