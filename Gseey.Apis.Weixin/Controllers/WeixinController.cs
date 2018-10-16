d13b5b4fde4a44a2931fc30df20ed9c050e69d84
高爽 <gaos@live.cn>
2018-10-16 16:27:04 +0800

    调整api接口

---
+++ Gseey.Apis.Weixin/Controllers/WeixinController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gseey.Apis.Weixin.Controllers
+{
+    [Route("api/Weixin")]
+    [ApiController]
+    public class WeixinController : ControllerBase
+    {
+        public ActionResult<IEnumerable<string>> Index()
+        {
+            return Content("fsdfdsfsdfsdfsf");
+        }
+    }
+}
